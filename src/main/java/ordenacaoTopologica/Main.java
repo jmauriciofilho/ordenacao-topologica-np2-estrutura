e2d8d757b399762078c1b3297bcfdd477915f2fc
Mauricio Pereira <jmfcientista@gmail.com>
2017-06-03 12:22:11 -0300

    entrada e saida commit

--- src/main/java/ordenacaoTopologica/Main.java
+++ src/main/java/ordenacaoTopologica/Main.java
@@ -8,15 +8,16 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
+        String fileName = "/home/mauricio/workspace/OrdenacaoTopologica/src/main/java/ordenacaoTopologica/entrada";
         InputStream input;
-       // try {
-            //input = new FileInputStream("entrada");
-            File file = new File("entrada");
-            System.out.println(file.canRead());
-           // processa(input);
-       // } catch (IOException e) {
-         //   e.printStackTrace();
-       // }
+       try {
+           input = new FileInputStream(fileName);
+//            File file = new File("entrada.txt");
+//            System.out.println(file.canRead());
+           processa(input);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
     }
 
     private static void processa(InputStream input) throws IOException {
@@ -38,7 +39,8 @@ public class Main {
     }
 
     private static void saida(int resultado) throws IOException {
-        OutputStream os = new FileOutputStream("saida");
+        String fileName = "/home/mauricio/workspace/OrdenacaoTopologica/src/main/java/ordenacaoTopologica/saida";
+        OutputStream os = new FileOutputStream(fileName);
         OutputStreamWriter osw = new OutputStreamWriter(os);
         BufferedWriter bw = new BufferedWriter(osw);
         bw.write(Integer.toString(resultado));
